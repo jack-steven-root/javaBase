128bf769d7651799a203dd9e870bedc1ab716f61
jack-steven-root <lyyl9001@163.com>
2018-01-24 21:26:22 +0800

    add somebit set

--- src/com/ynu/java/learn/base/chapter7/Bits.java
+++ src/com/ynu/java/learn/base/chapter7/Bits.java
@@ -4,6 +4,9 @@
  */
 package com.ynu.java.learn.base.chapter7;
 
+import java.util.BitSet;
+import java.util.Random;
+
 /**
  * @author root
  *
@@ -15,6 +18,14 @@ public class Bits {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Random rand = new Random();
+
+		// Take the LSB of nextInt()
+		byte bt = (byte) rand.nextInt();
+		BitSet bb = new BitSet();
+		for (int i = 7; i >= 0; i--) {
+
+		}
 
 	}
 
